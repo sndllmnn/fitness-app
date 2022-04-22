4192639df4917530e13472264fc888707dc555d6
Sina <user@MacBook-Air-de-User.local>
2022-04-22 20:29:43 +0200

    add placeholder const as comments in ProgramOverview.js, don't work yet

--- src/components/dashboard/ProgramOverview.jsx
+++ src/components/dashboard/ProgramOverview.jsx
@@ -1,10 +1,17 @@
 const ProgramOverview = () => 
+
   <div className="programOverviewBox">
     <h3 className="h3 h3--dashboard text--dashboard">Tag 2</h3>
     <h2 className="h2 h2--dashboard text--dashboard">Titel des Programms</h2>
-    <p className="p p--dashboard text--dashboard">26 Min. · Beweglichkeit</p>
+    <p className="p p--dashboard text--dashboard"> 26 Min. · Beweglichkeit </p>
 
   </div>
 
 
+
 export default ProgramOverview;
+
+/* const dayCount = 2;
+const programTitle = 'Titel des Programms';
+const workoutDurance = 26;
+const workoutFocus = 'Beweglichkeit'; */
