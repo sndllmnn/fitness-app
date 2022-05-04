8d88eb98f54000760fd0403639895ee6914d6122
Sina <user@MacBook-Air-de-User.local>
2022-05-04 18:27:27 +0200

    appollo setup, graphcms query implement, doesn't work, white screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,24 @@ import { useState } from "react";
 /* internal imports */
 
 import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const GET_WORKOUTS = gql`
+  query MyQuery {
+    workouts {
+      duration
+      name
+      id
+    }
+  }
+`;
+
 /* import programs from "./programs.json"; */
 
 import WelcomeScreen from "./components/dashboard/WelcomeScreen";
@@ -13,11 +31,23 @@ import NavigationIcons from "./components/generalComponents/Navigation/Navigatio
 import ProgramDetails from "./components/programme/ProgramDetails";
 
 function App() {
+  const { loading, error, data } = useQuery(GET_WORKOUTS);
+
+  if (loading) return <p>Lädt...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
   return (
     <>
       <body style={{ height: "100vh", background: "#202430" }}>
         <div className="dashboard">
-        <ProgramDetails />
+          <p>
+            {workouts.map((workout, i) => (
+              <div key={i}>{workout.name}</div>
+            ))}
+          </p>
+          <ProgramDetails />
           <WelcomeScreen username="Sina" />
           <CurrentProgram
             dayCount="2"
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
+
 import "./index.css";
 import App from "./App";
 
+const client = new ApolloClient({
+  uri: "https://api-eu-central-1.graphcms.com/v2/cko4j7ro2ig7j01yz5tfzd0fi/master", // richtige graphcms adresse einfügen
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
